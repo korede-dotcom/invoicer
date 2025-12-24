13ad0bf01d2df46ea6d607fc216be59cd0c5772c
koredebada <koredebada@gmail.com>
2025-12-24 15:52:52 +0100

    adding new features

--- src/modules/recurring-invoices/recurring-invoices.module.ts
+++ src/modules/recurring-invoices/recurring-invoices.module.ts
@@ -1,12 +1,14 @@
 import { JwtService } from '@nestjs/jwt';
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { RecurringInvoicesController } from '@/modules/recurring-invoices/recurring-invoices.controller';
 import { MailService } from '@/mail/mail.service';
 import { InvoicesService } from '@/modules/invoices/invoices.service';
 import { RecurringInvoicesCronService } from '@/modules/recurring-invoices/cron.service';
 import { RecurringInvoicesService } from '@/modules/recurring-invoices/recurring-invoices.service';
+import { PaymentModule } from '@/modules/payments/payment.module';
 
 @Module({
+  imports: [forwardRef(() => PaymentModule)],
   controllers: [RecurringInvoicesController],
   providers: [
     RecurringInvoicesService,
